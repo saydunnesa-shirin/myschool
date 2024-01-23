62688c505e99b99eab2c15854c2243aadfd6b8dd
Mamun Reza <mamun.reza09@gmail.com>
2024-01-23 10:37:24 +0200

    MYS-9 Some additional end points for type data

--- Api/Extensions/ServiceExtensions.cs
+++ Api/Extensions/ServiceExtensions.cs
@@ -6,58 +6,58 @@ namespace Api.Extensions;
 
 public static class ServiceExtensions
 {
-  public static void ConfigureCors(this IServiceCollection services) =>
-    services.AddCors(options =>
-    {
-      options.AddPolicy("CorsPolicy", builder =>
-      builder.AllowAnyOrigin()
-      .AllowAnyMethod()
-      .AllowAnyHeader());
-    });
-
-  public static void ConfigureSwagger(this IServiceCollection services)
-  {
-    services.AddSwaggerGen(options =>
-    {
-      options.SwaggerDoc("v1",
-        new OpenApiInfo
-        {
-          Title = "Customer Case Service API",
-          Description = "API microservice for CRM Customer Case Service",
-          Version = "v1"
-        });
-      options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-      options.CustomSchemaIds(type =>
-        type.ToString().Replace(type.Namespace + ".", "").Replace("+", "."));
-
-      options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    public static void ConfigureCors(this IServiceCollection services) =>
+      services.AddCors(options =>
       {
-        Description = "ApiKey must appear in header",
-        Type = SecuritySchemeType.ApiKey,
-        Name = "X-Api-Key",
-        In = ParameterLocation.Header,
-        Scheme = "ApiKeyScheme"
+          options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
       });
-      var key = new OpenApiSecurityScheme
-      {
-        Reference = new OpenApiReference
+
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
         {
-          Type = ReferenceType.SecurityScheme,
-          Id = "ApiKey"
-        },
-        In = ParameterLocation.Header
-      };
-      var requirement = new OpenApiSecurityRequirement
-  {
-    { key, new List<string>() }
-  };
-      options.AddSecurityRequirement(requirement);
+            options.SwaggerDoc("v1",
+              new OpenApiInfo
+              {
+                  Title = "Customer Case Service API",
+                  Description = "API microservice for CRM Customer Case Service",
+                  Version = "v1"
+              });
+            options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            options.CustomSchemaIds(type =>
+          type.ToString().Replace(type.Namespace + ".", "").Replace("+", "."));
 
-      var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-      options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-      options.ExampleFilters();
-    });
+            options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+            {
+                Description = "ApiKey must appear in header",
+                Type = SecuritySchemeType.ApiKey,
+                Name = "X-Api-Key",
+                In = ParameterLocation.Header,
+                Scheme = "ApiKeyScheme"
+            });
+            var key = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                In = ParameterLocation.Header
+            };
+            var requirement = new OpenApiSecurityRequirement
+              {
+                { key, new List<string>() }
+              };
+            options.AddSecurityRequirement(requirement);
 
-    services.AddSwaggerExamplesFromAssemblyOf(typeof(ServiceExtensions));
-  }
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            options.ExampleFilters();
+        });
+
+        services.AddSwaggerExamplesFromAssemblyOf(typeof(ServiceExtensions));
+    }
 }
---
+++ Api/Features/Commons/TypesController.cs
@@ -0,0 +1,83 @@
+﻿using Api.Common;
+using Api.Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Features.Commons;
+
+[ApiController]
+[Route("[controller]")]
+public class TypesController : Controller
+{
+    /// <remarks>
+    ///   Get list of designations
+    /// </remarks>
+    [HttpGet("designations")]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<TypeViewModel>))]
+    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.ServiceUnavailable, Type = typeof(ApiError))]
+    public ActionResult<List<TypeViewModel>> GetDesignationsAsync()
+    {
+        var list = Enum.GetValues(typeof(Designation))
+               .Cast<Designation>()
+               .Select(t => new TypeViewModel
+               {
+                   Id = ((int)t),
+                   Name = t.ToString()
+               })
+               .ToList();
+
+        return Ok(list);
+    }
+
+    /// <remarks>
+    ///   Get list of employee types
+    /// </remarks>
+    [HttpGet("employeetypes")]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<TypeViewModel>))]
+    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.ServiceUnavailable, Type = typeof(ApiError))]
+    public ActionResult<List<TypeViewModel>> GetEmployeeTypesAsync()
+    {
+        var list = Enum.GetValues(typeof(EmployeeType))
+               .Cast<EmployeeType>()
+               .Select(t => new TypeViewModel
+               {
+                   Id = ((int)t),
+                   Name = t.ToString()
+               })
+               .ToList();
+
+        return Ok(list);
+    }
+
+    /// <remarks>
+    ///   Get list of genders
+    /// </remarks>
+    [HttpGet("genders")]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<TypeViewModel>))]
+    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ApiError))]
+    [ProducesResponseType((int)HttpStatusCode.ServiceUnavailable, Type = typeof(ApiError))]
+    public ActionResult<List<TypeViewModel>> GetGendersAsync()
+    {
+        var list = Enum.GetValues(typeof(Gender))
+               .Cast<Gender>()
+               .Select(t => new TypeViewModel
+               {
+                   Id = ((int)t),
+                   Name = t.ToString()
+               })
+               .ToList();
+
+        return Ok(list);
+    }
+}
