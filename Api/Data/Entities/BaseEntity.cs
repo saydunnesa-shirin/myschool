5eda9a7a2235b8fd769ac2175669ade579e314a1
saydunnesa-shirin <shirin071989@gmail.com>
2024-03-12 15:19:29 +0200

    Added UpdateAcademicSessionWithDetails

--- Api/Data/Entities/BaseEntity.cs
+++ Api/Data/Entities/BaseEntity.cs
@@ -7,5 +7,5 @@ public class BaseEntity
     public int CreatedBy { get; set; }
     public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
     public int UpdatedBy { get; set; }
-    public bool IsActive { get; set; } = true;
+    public bool IsActive { get; set; } = false;
 }
